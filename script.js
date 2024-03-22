88bdb045bfda7ed2d62328dead273f3c1cc7c12f
BENOITMEZAGUER <161327017+BENOITMEZAGUER@users.noreply.github.com>
2024-03-22 11:35:58 +0100

    Correction sur toutes les parties

--- script.js
+++ script.js
@@ -98,16 +98,16 @@ function filterCity () {
 
 /* Menu mobile */ 
 
-let sidenav = document.getElementById("mySidenav");
-let openBtn = document.getElementById("openBtn");
-let closeBtn = document.getElementById("closeBtn");
+var sidenav = document.getElementById("mySidenav");
+var openBtn = document.getElementById("openBtn");
+var closeBtn = document.getElementById("closeBtn");
 
 openBtn.onclick = openNav;
 closeBtn.onclick = closeNav;
 
 /* Set the width of the side navigation to 250px */
 function openNav(event) {
-    event.preventDefault();
+  event.preventDefault();
   sidenav.classList.add("active");
 }
 
