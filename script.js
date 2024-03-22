9995dff0b0601c374ad43394950ab30eddc2a150
EmiLg <88514461+EmilieLgt@users.noreply.github.com>
2024-03-22 10:13:42 +0100

    ajout prevent default sur la fermeture du menu burger

--- script.js
+++ script.js
@@ -98,15 +98,16 @@ function filterCity () {
 
 /* Menu mobile */ 
 
-var sidenav = document.getElementById("mySidenav");
-var openBtn = document.getElementById("openBtn");
-var closeBtn = document.getElementById("closeBtn");
+let sidenav = document.getElementById("mySidenav");
+let openBtn = document.getElementById("openBtn");
+let closeBtn = document.getElementById("closeBtn");
 
 openBtn.onclick = openNav;
 closeBtn.onclick = closeNav;
 
 /* Set the width of the side navigation to 250px */
-function openNav() {
+function openNav(event) {
+    event.preventDefault();
   sidenav.classList.add("active");
 }
 
